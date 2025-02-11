4cdcad588efb66a07d65d66a51e457c97270677f
Pablo Leyes <n.leyes.n@gmail.com>
2025-02-11 17:28:26 -0300

    RestDoc Implementation on Test

--- src/test/java/guru/microservices/springdoc/web/controller/BeerControllerTest.java
+++ src/test/java/guru/microservices/springdoc/web/controller/BeerControllerTest.java
@@ -20,7 +20,10 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -40,8 +43,11 @@ class BeerControllerTest {
     @Test
     void getBeerById() throws Exception {
 
-        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("/v1/beer", pathParameters(
+                        parameterWithName("beerId").description("UUID of desired beer to get")
+                )));
     }
 
     @Test
